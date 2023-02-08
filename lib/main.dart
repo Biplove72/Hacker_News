11ff573ed3682abc77a60b16f25211d7a242fa25
unknown <you@example.com>
2023-02-08 13:49:00 +0545

    first add

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import'package:flutter/material.dart';
+
+
+    void main(){
+  runApp(HackerNews());
+
+
+    }
+
+
+    class HackerNews extends StatelessWidget {
+      const HackerNews({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context) {
+        return const Placeholder();
+      }
+    }
